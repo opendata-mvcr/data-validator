3b28ede15cddf846bd992c8fbc87805ec920d9e7
Petr Škoda <skodapetr@gmail.com>
2020-09-02 11:20:34 +0200

    Improve SHACL validation message

--- data-validator-rdf/src/main/java/cz/mvcr/datavalidator/rdf/schema/RdfSchemaShaclJenaValidator.java
+++ data-validator-rdf/src/main/java/cz/mvcr/datavalidator/rdf/schema/RdfSchemaShaclJenaValidator.java
@@ -6,12 +6,20 @@ import cz.mvcr.datavalidator.core.Report;
 import cz.mvcr.datavalidator.core.ReportFactory;
 import cz.mvcr.datavalidator.rdf.syntax.JsonLdTitaniumLoader;
 import org.apache.jena.graph.Graph;
+import org.apache.jena.graph.GraphUtil;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.mem.GraphMem;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
 import org.apache.jena.shacl.ShaclValidator;
 import org.apache.jena.shacl.Shapes;
 import org.apache.jena.shacl.ValidationReport;
 import org.apache.jena.shacl.engine.constraint.ShNode;
 import org.apache.jena.shacl.parser.Constraint;
+import org.apache.jena.shacl.parser.Shape;
 import org.apache.jena.shacl.validation.ReportEntry;
 import org.apache.jena.shacl.validation.Severity;
 import org.apache.jena.sparql.core.Prologue;
@@ -72,6 +80,7 @@ public class RdfSchemaShaclJenaValidator implements ConfigurableValidator {
         }
         ValidationReport reports =
                 ShaclValidator.get().validate(shapes, fileContent);
+
         List<Report> result = new ArrayList<>();
         for (ReportEntry entry : reports.getEntries()) {
             Report.Type type = getType(entry.severity());
@@ -108,13 +117,25 @@ public class RdfSchemaShaclJenaValidator implements ConfigurableValidator {
 
     private Report createReport(ReportEntry entry, Report.Type type) {
         String focus = entry.focusNode().getURI();
-        String pathStr = entry.resultPath().toString(new Prologue());
+        String path = entry.resultPath().toString(new Prologue());
         String value = entry.value().toString();
-        // Node at focusNode <https://linked.cuzk.cz/resource/ruian/stat/1>
-        return reportFactory.message(
-                type,
-                "NodeShape constraint failed for <"
-                        + focus + "> " + pathStr + " " + value);
+        String message =
+                "Constraints " + String.join(" ", getShapeDescriptions(entry))
+                        + " dos not hold for "
+                        + "<" + focus + "> <" + path + "> " + value + " .";
+        return reportFactory.message(type, message);
+    }
+
+    private List<String> getShapeDescriptions(ReportEntry entry) {
+        var iter = shapes.getGraph().find(
+                entry.source(),
+                NodeFactory.createURI("http://www.w3.org/ns/shacl#description"),
+                Node.ANY);
+        List<String> result = new ArrayList<>();
+        while (iter.hasNext()) {
+            result.add(iter.next().getObject().toString());
+        }
+        return result;
     }
 
 }
